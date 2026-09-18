7182ec17effdd1b588d37c1b33529796857f1a5b
Aung Nyi Nyi Win <aungnyinyiwin37@gmail.com>
2026-09-18 13:47:21 +0630

    loginsystemV1.py

--- loginsystemV1.py
+++ loginsystemV1.py
@@ -1,84 +1,109 @@
 users = {
-    "aung":"12345",
-    "admin":"admin123"
+    "aung123":"123456"
 }
+
+def login(users):
+    username = input("enter username:").strip().lower()
+    saved_password = users.get(username)
+    if saved_password is None:
+        print("wrong username.")
+        return False,None
+    attempts = 0
+    while attempts < 3:
+        password = input("enter password.").strip()
+        if saved_password == password:
+            return True,username
+        else:
+            attempts += 1
+            print("wrong password.")
+    if attempts == 3:
+        print("account locked.")
+        return False,None
+
+def get_username(users):
+    while True:
+        new_username = input("enter new username:").strip().lower()
+        if new_username in users:
+            print("username already exists.")
+            continue
+        if len(new_username) < 6:
+            print("please enter at least 6 characters")
+            continue
+        return new_username
+
+def get_password():
+    while True:
+        new_password = input("enter new password:").strip()
+        if len(new_password) <6:
+            print("please enter at least 6 characters")
+        else:
+            break
+    while True:
+        confirm_password = input("enter confirm password.").strip()
+        if new_password != confirm_password:
+            print("passwords do not match.")
+        else:
+            break
+    return new_password
+
+def register(users):
+    new_username = get_username(users)
+    new_password = get_password()
+    users[new_username]=new_password
+    return True
+
+def change_password(users,username):
+    old_password = input("enter old password.").strip()
+    if old_password != users[username]:
+        print("old password not found.")
+        return False
+    else:
+        new_password = get_password()
+        users[username]=new_password
+        return True
+
+def user_menu(users,username):
+    while True:
+        print("===user menu===")
+        print("1. username.")
+        print("2. change password")
+        print("3. logout.")
+        users_choice = input("enter user choice:")
+        if users_choice =="1":
+            print("welcome,",username)
+        elif users_choice == "2":
+            changed = change_password(users,username)
+            if changed:
+                print("password changed.")
+            else:
+                print("failed ")
+        elif users_choice == "3":
+            print("logout")
+            break
+        else:
+            print("invalid users choice.")
 
 while True:
     print("\n===login system===")
     print("1. login")
     print("2. register")
-    print("3. exit")
+    print("3. exit.")
     choice = input("enter choice:")
     if choice == "1":
-        username = input("enter username:").strip().lower()
-        saved_password = users.get(username)
-        if saved_password is None:
-            print("wrong username.")
-            continue
-        attempts = 0
-        while attempts < 3:
-            password = input("enter password:").strip()
-            if saved_password == password:
-                print("login success.")
-                while True:
-                    print("\n===user menu===")
-                    print("1. username")
-                    print("2. change password.")
-                    print("3. logout.")
-                    user_choice= input("enter choice:")
-                    if user_choice == "1":
-                        print("welcome,",username)
-                    elif user_choice=="2":
-                        old_password = input("enter Old PW:").strip()
-                        if old_password == users[username]:
-                            new_password = input("enter New PW:").strip()
-                            users[username]=new_password
-                            print("changed password.")
-                        else:
-                            print("old password do not found")
-                    elif user_choice=="3":
-                        print("successfully logout")
-                        break
-                    else:
-                        print("invalid user choice.")
-                break
-            else:
-                attempts += 1
-                print("wrong password.")
-        if attempts == 3:
-            print("account locked.")
+        success,username = login(users)
+        if success:
+            print("login success.")
+            user_menu(users,username)
+        else:
+            print("try again later")
     elif choice == "2":
-        
-        
-        while True:
-            new_username = input("enter new username:").strip().lower()
-            if new_username in users:
-                print("username already exist.")
-            else:
-                if len(new_username) <6:
-                    print("please enter username at least 6 characters")
-                else:
-                    break
-            
-        
-                
-        while True:
-            new_password = input("enter new password").strip()
-            if len(new_password) <6:
-                print("please add password at least 6 character.")
-
-            else:
-                
-            
-                confirm_password = input("enter confirm password").strip()
-                if new_password != confirm_password:
-                    print("password do not match.")
-                else:
-                    users[new_username]= new_password
-                    print("registration success")
-                    break
-    elif choice =="3":
-        print("good bye")
+        passed = register(users)
+        if passed:
+            print("registration success.")
+        else:
+            print("registration failed.")
+    elif choice == "3":
+        print("exit")
         break
     else:
-        print("invalid choice.")
+        print("invaild choice.")
